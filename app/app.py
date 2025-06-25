eee05f94d579ebaf939e5d897da72f4de9009612
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-06-25 17:34:08 -0300

    Criando as configs

--- app/app.py
+++ app/app.py
@@ -1,7 +1,18 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
+from routes.home import home
+from utils.db import db
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+#Chaves de configuração em um arquivo .env
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Inicializando o banco de dados
+db.init_app(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(home, url_prefix='/')
