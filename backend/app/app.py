01347e4008a6381bec87e6b5183d31043731c9fe
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-11-13 21:30:28 -0300

    Ajustando importação do arquivo Auth antes Cadastro e Login | Utilização do Dotenv para buscar os dados secretos no .env.

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,18 +2,23 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from blueprints.home import home
-from blueprints import cadastro, login
+from blueprints import auth
 from utils.db import db
+from flask_cors import CORS
+import dotenv
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 #Chaves de configuração em um arquivo .env
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config['SECRET_KEY'] = dotenv.get_key(dotenv.find_dotenv(), 'SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = dotenv.get_key(dotenv.find_dotenv(), 'DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Inicializando o banco de dados
 db.init_app(app)
 migrate = Migrate(app, db)
 
-app.register_blueprint(home, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/api/auth')
+app.register_blueprint(home, url_prefix='/api/dashboard')
+
