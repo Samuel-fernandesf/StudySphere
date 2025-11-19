98888dc6df17b7e1659107e6771ed4176042bf3b
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-11-19 11:53:34 -0300

    Métodos de envio e criptografia de Tokens para os emails | Funçõ de envio de email.

--- backend/app/models/usuario.py
+++ backend/app/models/usuario.py
@@ -15,9 +15,63 @@ class Usuario(db.Model, UserMixin):
     nome_completo = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     nascimento = db.Column(db.Date, nullable=True)
+    confirm_user = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
     revoked_tokens = db.relationship('RevokedToken', back_populates='user')
+    
+    def get_confirmation_token(self):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    
+        hora_utc = datetime.now(timezone.utc)
+        timestamp = hora_utc.timestamp()
+
+        return s.dumps({'user_id': self.id, 'timestamp': timestamp})
+    
+    @staticmethod # Não precisa de self, pois não depende de nenhuma instância
+    def verify_confirmation_token(token, expires_sec=86400):#Expira em 24 horas
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+
+        try:
+            dados = s.loads(token, max_age=expires_sec)
+            user_id = dados['user_id']
+            timestamp = int(dados['timestamp'])
+        except:
+            return None
+        
+        #Pegar o id do usuario que realizará a confirmação
+        user= Usuario.query.get(user_id)
+
+        if user is None:
+            return None
+
+        return user
+
+    def get_reset_token(self):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+
+        hora_utc = datetime.now(timezone.utc)
+        timestamp = hora_utc.timestamp()
+
+        return s.dumps({'user_id': self.id, 'timestamp': timestamp})
+    
+    @staticmethod # não precisa de um objeto (instância). Útil para funções relacionadas à classe, mas que não usam 'self'. Pertence à classe, mas não depende de nenhuma instância específica dela, como se fosse uma função de fora do objeto basicamente.
+    def verify_reset_token(token, expires_sec=1800):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        try:
+            dados = s.loads(token, max_age=expires_sec)
+            user_id = dados['user_id']
+            timestamp = int(dados['timestamp'])
+        except:
+            return None
+        
+        #Pegar o id do usuario que solicitou a redefinição
+        user= Usuario.query.get(user_id)
+
+        if user is None:
+            return None
+
+        return user
     
     @property
     def cripto_pwd(self):
