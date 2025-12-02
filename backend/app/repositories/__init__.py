b9f15889124d96479f09143bfe74d52aea868c66
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-12-02 07:46:44 -0300

    Cria os métodos de busca, cadastro, atualização e deleção dos modelos de dados.

--- backend/app/repositories/__init__.py
+++ backend/app/repositories/__init__.py
@@ -1,5 +1,9 @@
 from .user_repo import UserRepository
 from .token_repo import TokenRepository
+from .chat_repo import ChatRepository, MessageRepository, ChatUsuarioRepository
 
 userRepository = UserRepository()
 tokenRepository = TokenRepository()
+chatRepository = ChatRepository()
+messageRepository = MessageRepository()
+chatUsuarioRepository = ChatUsuarioRepository()
